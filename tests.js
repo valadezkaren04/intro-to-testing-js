b96d0b55f8c865ccd3a3758052e924e6714aacaf
Karen Valadez <valadez.karen04@gmail.com>
2021-03-24 17:39:03 -0500

    exercise three complete of the intro-to-testing-js

--- tests.js
+++ tests.js
@@ -21,6 +21,8 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe('string');
     });
-
+    it('should return "Hello, Jane!" when executed', function() {
+        expect(sayHello()).toBe("Hello, Jane!");
+    });
 });
 
